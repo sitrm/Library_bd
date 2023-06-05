6143d76e59e439019f997ea3dd517097c8f96629
DV <pit@mail.ru>
2023-06-05 21:20:51 +0300

    Идея с фонами фигня..Нужно подгонять размеры изображени и окна. Текст на изображении видно плохо

--- books.py
+++ books.py
@@ -1,11 +1,33 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QPushButton, QTextEdit, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QPushButton, QTextEdit
 import sqlite3
+from PyQt6.QtGui import QPalette, QPixmap
+from PyQt5.QtCore import Qt
 
 class BooksWin(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.setStyleSheet("""
+            QWidget {
+                background-color: #333333;
+                color: #ffffff;
+            }
+            QPushButton {
+                background-color: #555555;
+                color: #ffffff;
+                border: none;
+                padding: 5px;
+            }
+            QPushButton:hover {
+                background-color: #666666;
+            }
+            QLineEdit {
+                background-color: #444444;
+                color: #ffffff;
+            }
+        """)
+
 
         # создаем соединение с нашей базой данных
         self.conn = sqlite3.connect('library.db')
@@ -15,6 +37,7 @@ class BooksWin(QMainWindow):
 
         # создаем графический интерфейс
         self.setWindowTitle('Библиотека')
+
         self.setGeometry(100, 100, 500, 500)
 
         # создаем виджеты для ввода данных
@@ -45,6 +68,7 @@ class BooksWin(QMainWindow):
         # создаем кнопки для добавления и вывода книг
         self.add_button = QPushButton('Добавить книгу', self)
         self.add_button.move(20, 180)
+        self.add_button.setGeometry(20, 180, 120, 30)
         self.add_button.setStyleSheet('background: rgb(255,0,0);')
         self.add_button.setStyleSheet("""
         QPushButton{
@@ -60,6 +84,7 @@ class BooksWin(QMainWindow):
 
         self.show_button = QPushButton('Показать книги', self)
         self.show_button.move(150, 180)
+        self.show_button.setGeometry(150, 180, 120, 30)
         self.show_button.setStyleSheet("""
         QPushButton{
             font-style: oblique;
@@ -75,7 +100,7 @@ class BooksWin(QMainWindow):
 
         # создаем текстовый виджет для вывода списка книг
         self.output = QTextEdit(self)
-        self.output.setGeometry(20, 220, 350, 250)
+        self.output.setGeometry(20, 220, 420, 250)
 
 
     # функция для добавления книги в базу данных
@@ -112,10 +137,20 @@ class BooksWin(QMainWindow):
         # закрываем соединение с базой данных при закрытии приложения
         self.conn.close()
 
+stylesheet = """
+    BooksWin {
+        background-image: url("D:\imag\lib.jpg"); 
+        background-repeat: no-repeat; 
+        background-position: center;
+    }
+"""
+
 if __name__ == '__main__':
     app = QApplication(
         sys.argv)  # создаем экзэмпляр класса. аргументы sys.argv используются для инициализации QApplication
     book_win = BooksWin()  # создаем обьект класс
+    app.setStyleSheet(stylesheet)
+
     book_win.show()  # выводим его на экран
     sys.exit(app.exec())  # sys.exit() гарантирует, что приложение завершится
     # корректно при завершении цикла обработки событий(app.exec_()).
