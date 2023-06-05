e401ffea92488595b4d63bb3c79994d46ef51e9b
DV <pit@mail.ru>
2023-06-05 23:23:32 +0300

    Добавил кнопку удаления пользователя и вывод пользователей отсортированных по рангу

--- users.py
+++ users.py
@@ -15,7 +15,7 @@ class UsersWin(QMainWindow):
 
         # создаем графический интерфейс
         self.setWindowTitle('Пользователи')
-        self.setGeometry(100, 100, 500, 600)  #position: x,y len: x,y
+        self.setGeometry(100, 100, 800, 600)  #position: x,y len: x,y
 
         # -----------------------------------------виджеты------------------------------------------
         self.name_label = QLabel('ФИ(Иванов И):', self)
@@ -23,12 +23,6 @@ class UsersWin(QMainWindow):
         self.name_entry = QLineEdit(self, placeholderText='name')
         self.name_entry.move(150, 100)
 
-        #TODO:зачем выданная книга?
-
-        # self.book_label = QLabel('Выданная книга:', self)
-        # self.book_label.move(20, 100)
-        # self.book_entry = QLineEdit(self, placeholderText='books')
-        # self.book_entry.move(150, 100)
 
         self.rang_label = QLabel('Ранг:', self)  # TODO: auto rang
         self.rang_label.move(20, 140)
@@ -40,9 +34,26 @@ class UsersWin(QMainWindow):
         self.phone_entry = QLineEdit(self, placeholderText='phone')
         self.phone_entry.move(150, 180)
 
+        self.delete_button = QPushButton('Удалить читателя', self)
+
+        self.delete_button.setGeometry(380, 108, 150, 30)
+        self.delete_button.setStyleSheet('background: rgb(255,0,0);')
+        self.delete_button.setStyleSheet("""
+        QPushButton{
+            font-style: oblique;
+            font-weight: bold;
+            border: 1px solid #1DA1F2;
+            border-radius: 15px;
+            color: #1DA1F2;
+            background-color: #fff;
+        }
+        """)
+        self.delete_entry = QLineEdit(self, placeholderText='id читателя')
+        self.delete_entry.move(550, 100)
+        self.delete_button.clicked.connect(self.delete_user)
+
         # -------------------------------------------кнопки------------------------------------------------
         self.add_button = QPushButton('Добавить читателя', self)
-        self.add_button.move(20, 240)
         self.add_button.setGeometry(20, 240, 150, 30)
         self.add_button.setStyleSheet('background: rgb(255,0,0);')
         self.add_button.setStyleSheet("""
@@ -59,7 +70,6 @@ class UsersWin(QMainWindow):
 
         self.show_button = QPushButton('Показать читателей', self)
         self.show_button.setGeometry(200, 240, 150, 30)
-        self.show_button.move(200, 240)
         self.show_button.setStyleSheet("""
         QPushButton{
             font-style: oblique;
@@ -71,6 +81,20 @@ class UsersWin(QMainWindow):
         }
         """)
         self.show_button.clicked.connect(self.show_readers)
+
+        self.sort_button = QPushButton('Отсортировать по рангу', self)
+        self.sort_button.setGeometry(20, 280, 170, 30)
+        self.sort_button.setStyleSheet("""
+        QPushButton{
+            font-style: oblique;
+            font-weight: bold;
+            border: 1px solid #1DA1F2;
+            border-radius: 15px;
+            color: #1DA1F2;
+            background-color: #fff;
+        }
+        """)
+        self.sort_button.clicked.connect(self.sort_user_rang)
 
         # создаем текстовый виджет для вывода списка людей
         self.output = QTextEdit(self)
@@ -91,6 +115,7 @@ class UsersWin(QMainWindow):
         self.name_entry.setText('')
         self.rang_entry.setText('')
         self.phone_entry.setText('')
+        self.output.clear()
         self.output.append(f'Пользователь {name} успешно добавлен!')
     # функция для вывода списка читателей из базы данных
     def show_readers(self):
@@ -104,6 +129,22 @@ class UsersWin(QMainWindow):
             self.output.append(f'{r[0]}.  {r[1]}, {r[2]}, {r[3]};')
 
     #удалить пользователя
+    def delete_user(self):
+        user_id = self.delete_entry.text()
+        name_user = self.cur.execute('SELECT name FROM users WHERE user_id = ?', (user_id,)).fetchall()[0][0]
+        self.cur.execute("DELETE FROM users WHERE user_id = ?", (user_id,))
+        self.conn.commit()
+        self.delete_entry.setText('')
+        self.cur.execute('SELECT name FROM users WHERE user_id = ?', (user_id,))
+        self.output.clear()
+        self.output.append(f"Пользователь {name_user} успешно удален!")
+
+    def sort_user_rang(self):
+        sort_user = self.cur.execute("SELECT * FROM users ORDER BY rang DESC")
+        self.output.clear()
+        for cur_sort_user in sort_user:
+            self.output.append(f"{cur_sort_user[0]}. {cur_sort_user[1]}, {cur_sort_user[2]}, {cur_sort_user[3]}")
+
 
     def closeEvent(self, event):
         # закрываем соединение с базой данных при закрытии приложения
